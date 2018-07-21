9c7ea1318c84dfdddf75163e1c0db910a38a3ca5
serjpetrenko <b.g.lupus@gmail.com>
2018-07-21 18:35:49 +0300

    Change synthax install new version of bundler

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   include Knock::Authenticable
 
   private
-  
+
   def render_403
     render file: "public/403.html", status: 403
   end
@@ -12,5 +12,4 @@ class ApplicationController < ActionController::Base
   def render_404
     render file: "public/404.html", status: 404
   end
-
 end
