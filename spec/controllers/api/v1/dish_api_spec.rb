4cfbf27b263f45353f523bd8eb9f303846ce356a
serjpetrenko <b.g.lupus@gmail.com>
2018-07-21 18:54:31 +0300

    Cover index dishes, add helper for requests

--- spec/controllers/api/v1/dish_api_spec.rb
+++ spec/controllers/api/v1/dish_api_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 describe API::V1::DishAPI, type: :request do
-  include Rack::Test::Methods
+  let(:dish) { create(:dish) }
 
-  let(:user) { create(:user) }
-  
   before do
-    UsersHelper.authorize(user)
+    allow(UsersHelper).to receive(:authorize).and_return(true)
     header 'Content-Type', 'application/vnd.api+json'
   end
 
   describe 'GET /api/v1/dishes' do
-    it 'returns HTTP status 200' do
+    before do
+      dish
       get '/api/v1/dishes'
+      json
+    end
+
+    it 'returns HTTP status 200' do
       expect(last_response.status).to eq 200
+    end
+
+    it 'returns all dishes' do
+      expect(json.size).to eq(1)
     end
   end
 end
